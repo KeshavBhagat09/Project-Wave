84d02ce0bf43e9abccf84bfb9ad5ab9e5da946c2
Keshav Bhagat <sendemailtokeshav@gmail.com>
2024-11-30 22:08:47 +0530

    Making a button reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import ButtonGradient from './assets/svg/ButtonGradient';
-import Button from './Components/Button';
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./Components/Button";
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className ="mt-10" href="#login">
-          Something
-        </Button>
+        <Button className="mt-10">Something</Button>
       </div>
-      <ButtonGradient/>
+      <ButtonGradient />
     </>
   );
 };
