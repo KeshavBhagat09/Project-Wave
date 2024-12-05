acd77675b95e6d1bb60062b85cf0514cc4c35f25
Keshav Bhagat <sendemailtokeshav@gmail.com>
2024-12-05 18:03:40 +0530

    Complete the pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Benefits from "./Components/Benefits";
 import Collaboration from "./Components/Collaboration";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import Pricing from "./Components/Pricing";
 import Services from "./Components/Services";
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Services/>
       </div>
       <ButtonGradient />
+      <Pricing/>
     </>
   );
 };
